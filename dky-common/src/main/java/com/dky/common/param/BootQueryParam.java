9b5f66769a0bda7306d28f2836eaf6c1bb7d6820
huangyihang <12345678>
2017-01-08 16:55:18 +0800

    1、默认参数

--- dky-common/src/main/java/com/dky/common/param/BootQueryParam.java
+++ dky-common/src/main/java/com/dky/common/param/BootQueryParam.java
@@ -1,13 +1,13 @@
 package com.dky.common.param;
 
-import com.dky.common.session.SessionParameter;
+import com.dky.common.session.BaseParameter;
 
 
 /**
  * 启动页查询对象
  * Created by wonpera on 2017/1/4.
  */
-public class BootQueryParam extends SessionParameter {
+public class BootQueryParam extends BaseParameter {
 
 
 }
